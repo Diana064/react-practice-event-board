752627ccb9e01565a8b9e96c7af27f8acce211b6
Diana064 <dipustolyakova02@gmail.com>
2022-12-12 18:54:14 +0200

    add commit

--- src/components/pageTitle/PageTitle.styled.jsx
+++ src/components/pageTitle/PageTitle.styled.jsx
@@ -6,10 +6,3 @@ export const Title = styled.h1`
   text-transform: uppercase;
   color: ${props => `${props.theme.colors.black}`};
 `;
-
-/* .title {
-  margin-top: 0;
-  text-align: center;
-  text-transform: uppercase;
-  color: pink;
-} */
